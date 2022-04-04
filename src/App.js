d298ca9dc838dcaa29613ef1102dbddc08a5a6a1
Daniyar <shergazievdan@gmail.com>
2022-04-04 19:43:28 +0600

    posts-add-delete

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import React, { useRef, useState } from 'react';
+import React, { useState } from 'react';
+import PostForm from './components/PostForm';
 
 // import Counter from './components/Counter';
 // import ClassCount from './components/ClassCounter'
 // import Input from './components/Input';
 import PostList from './components/PostList';
-import MyButton from './components/UI/button/MyButton';
-import MyInput from './components/UI/input/MyInput';
+// import MyButton from './components/UI/button/MyButton';
+// import MyInput from './components/UI/input/MyInput';
 import './styles/App.css'
 
 
@@ -14,22 +15,17 @@ function App() {
   const [posts, setPosts] = useState([
     { id: 1, title: 'React.js', body: 'Description' },
     { id: 2, title: 'java', body: 'Description' },
-    { id: 3, title: 'cotlin', body: 'Description' }
+    { id: 3, title: 'cotlin', body: 'Description' },
+    { id: 4, title: 'python', body: 'Description' },
+    { id: 5, title: 'PHP', body: 'Description'}
   ])
 
-  const [posts2, setPosts1] = useState([
-    { id: 1, title: 'python', body: 'Description' },
-    { id: 2, title: 'PHP', body: 'Description'}
-  ])
+  const createPost = (newPost) => {
+    setPosts([...posts, newPost])
+  }
 
-  const [title, setTitle] = useState('')
-  
-  const bodyInputRef = useRef()
-
-  const addNewPost = (e) => {
-    e.preventDefault()
-    console.log(title)
-    console.log(bodyInputRef.current.value)
+  const removePost = (post) => {
+    setPosts(posts.filter(p => p.id !== post.id))
   }
 
 
@@ -39,25 +35,15 @@ function App() {
         <Counter/>
         <ClassCount /> 
         <br/> */}
-      <form>
-        {/* Управляемый компонент */}
-          <MyInput
-            value={title}
-            onChange={e => setTitle(e.target.value)}
-            type="text"
-            placeholder="Название поста"
-        />
-        {/* Неуправляемый/неконтролируемый компонент  */}
-          <MyInput
-            ref={bodyInputRef}
-            type="text"
-            placeholder="Описание поста"
-          />
-            <MyButton onClick = {addNewPost}>Создать пост</MyButton>
-        </form>
-
-        <PostList posts={posts} title="Стэк 1" />
-        <PostList posts={posts2} title="Стэк 2"/>
+      <PostForm create={createPost} />
+      {posts.length
+        ? <PostList remove={removePost} posts={posts} title="Стэк технологий" />
+        : <h1
+          style={{ textAlign: 'center' }}>
+            Посты не найдены
+        </h1>
+      }
+        
     </div>
   );
 }
--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import Posts from './Posts';
 
-const PostList = ({posts, title}) => {
+const PostList = ({posts, title, remove}) => {
    return (
       <div>
          <h1 style={{ textAlign: 'center' }}>
             {title}
          </h1>
-         {posts.map((post) => 
-            <Posts post={post} key={post.id}/>
+         {posts.map((post, index) => 
+            <Posts remove={remove} number={index + 1} post={post} key={post.id}/>
          )}
       </div>
    );
