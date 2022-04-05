7db4586fd50a8def75481fe601c0bedff1338164
Daniyar <shergazievdan@gmail.com>
2022-04-05 17:11:19 +0600

    hook-error_loader

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import MyModal from './components/UI/MyModal/MyModal';
 import MyButton from './components/UI/button/MyButton';
 import { usePosts } from './hooks/usePosts';
 import PostService from './API/postServise';
+import { useFetching } from './hooks/useFetching';
 
 import './styles/App.css'
 
@@ -17,7 +18,10 @@ function App() {
   const [filter, setFilter] = useState({ sort: '', query: '' });
   const [modal, setModal] = useState(false);
   const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query);
-  const [isPostLading, setIsPostLoading] = useState(false)
+  const [fetchPosts, isPostLading, postError] = useFetching(async () => {
+    const posts = await PostService.getAll();
+    setPosts(posts)
+  })
 
   useEffect(() => {
     fetchPosts()
@@ -26,13 +30,6 @@ function App() {
   const createPost = (newPost) => {
     setPosts([...posts, newPost]);
     setModal(false)
-  }
-
-  async function fetchPosts() {
-    setIsPostLoading(true)
-    const posts = await PostService.getAll();
-    setPosts(posts)
-    setIsPostLoading(false);
   }
 
   const removePost = (post) => {
@@ -53,6 +50,8 @@ function App() {
         filter={filter}
         setFilter={setFilter}
       />
+      {postError && 
+        <h1>Произошла ошибка ${ postError } </h1>}
       {isPostLading
         ? <div style={{display: 'flex', justifyContent: 'center', marginTop: 50}}><Loader/></div>
         :<PostList remove={removePost} posts={sortedAndSearchedPosts} title="Стэк технологий" />
